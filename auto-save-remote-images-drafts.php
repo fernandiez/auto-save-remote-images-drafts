7bdeb0b92fe474c2a1b0583f44472d11f29acf0b
fernandiez <mail@fernan.com.es>
2015-06-20 01:06:46 +0200

    First commit
    First commit of the plugin, PH and readme files.

---
+++ auto-save-remote-images-drafts.php
@@ -0,0 +1,60 @@
+<?php  
+/*  
+Plugin Name: Auto Save Remote Image (Drafts Edition)
+Plugin URI: http://www.fernan.com.es/wordpress/
+Description: This plugin  downloads automatically the first remote image from a post and sets it as the featured image (but only when the post is saved as a draft, nver when publishing). Based on Auto Save Remote Image plugin by Prebhdev Singh http://www.devsaab.com/wordpress/ 
+Version: 1.0
+Author: Fernan Díez 
+Disclaimer: No warranty or guarantee of any kind!  Use this in your own risk.  
+*/
+add_action('save_post', 'fetch_images');
+
+function fetch_images( $post_ID )  
+{	
+	//Check to make sure function is not executed more than once on save
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+	return;
+
+	if ( !current_user_can('edit_post', $post_ID) ) 
+	return;
+
+	remove_action('save_post', 'fetch_images');	
+		
+	$post = get_post($post_ID);   
+
+	$first_image = '';
+	
+	if(preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches)){
+		$first_image = $matches [1] [0];
+	}
+
+	if (strpos($first_image,$_SERVER['HTTP_HOST'])===false)
+	{
+			
+		//Fetch and Store the Image	
+		$get = wp_remote_get( $first_image );
+		$type = wp_remote_retrieve_header( $get, 'content-type' );
+		$mirror = wp_upload_bits(rawurldecode(basename( $first_image )), '', wp_remote_retrieve_body( $get ) );
+	
+		//Attachment options
+		$attachment = array(
+		'post_title'=> basename( $first_image ),
+		'post_mime_type' => $type
+		);
+		
+		// Add the image to your media library and set as featured image
+		$attach_id = wp_insert_attachment( $attachment, $mirror['file'], $post_ID );
+		$attach_data = wp_generate_attachment_metadata( $attach_id, $first_image );
+		wp_update_attachment_metadata( $attach_id, $attach_data );
+		set_post_thumbnail( $post_ID, $attach_id );
+	
+		$updated = str_replace($first_image, $mirror['url'], $post->post_content);
+	    
+	    //Replace the image in the post
+	    wp_update_post(array('ID' => $post_ID, 'post_content' => $updated));
+	
+	    // re-hook this function
+	    add_action('save_post', 'fetch_images');		
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Auto Save Remote Image (Save Edition) ===
+Contributors: Fernan Díez
+Donate link: http://www.fernan.com.es/wordpress/
+Tags: auto download, remote image, featured, attachment, save, draft, hotlink, images
+Requires at least: 3.0
+Tested up to: 4.2.2
+Stable tag: 1.0
+
+WordPress plugin for downloading automatically first remote image from a post and setting it a featured image (only when the post is saved as a draft).
+
+Based on Auto Save Remote Image plugin by Prebhdev Singh
+http://www.devsaab.com/wordpress/ 
+
+== Description ==
+
+The purpose of this plugin is very simple - when a post is saved (not published), it will fetch the first remote/external image that is referenced.  The image that is retrieved is then attached to the post as the featured image.  There are no settings to configure.
+
+Please do not use this plugin to violate copyrights.
+
+Sample uses: You maintain multiple blogs; copy paste the post contents from one blog to the other and this plugin will automatically save the first external image to the second blog.  There is no need to re-upload images to multiple sites.
+
+This plugin can also retrieve and save images from sites like Flickr, Facebook, Picasa, etc.
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
